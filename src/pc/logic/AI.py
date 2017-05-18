384fd606b4d25c510b73e6b1192772f04a1e94cb
hawker <m.m.hawker@gmail.com>
2017-05-18 16:31:30 +0200

    add documentation for AI

--- src/pc/logic/AI.py
+++ src/pc/logic/AI.py
@@ -4,16 +4,25 @@ from clients.steering_client import SteeringClient
 import numpy as np
 from logic.action import Action
 
+
 class AI(metaclass=ABCMeta):
 	def __init__(self, model):
 		self.model = model
 
 	@abstractmethod
-	def prepare_input(self, img):
+	def prepare_input(self, img: np.array) -> np.array:
+		"""
+		:param img: an opencv image
+		:returns: an input for self.model.predict
+		"""
 		pass
 
 	@abstractmethod
-	def translate(self, prediction):
+	def translate(self, prediction: np.array) -> np.array:
+		"""
+		:param prediction: numpy array, result of self.modelpredict
+		:return: numpy bool array
+		"""
 		pass
 
 	def get_action(self, img):
@@ -35,29 +44,26 @@ class OpencvAI(AI, Action):
 		img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 		img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 		input = img[:, :].flatten()
-		input = np.float32(np.reshape(input,newshape=(1,19200)))
+		input = np.float32(np.reshape(input, newshape=(1, 19200)))
 		return input
 
 	def translate(self, prediction):
 		ret, resp = prediction
 		resp = resp[0]
-		resp[resp>0] = 1
-		resp[resp<0] = 0
+		resp[resp > 0] = 1
+		resp[resp < 0] = 0
 		return resp.astype(bool)
 
 	def action(self, frame, keys, **kwargs):
 		if keys[17]:
-			kwargs['keys']=keys
+			kwargs['keys'] = keys
 			return frame, kwargs
 		else:
 			new_keys = self.get_action(frame)
-			#print(new_keys)
+			# print(new_keys)
 			response = {}
 			for letter, value in zip(self.key_sequence, new_keys):
 				response[SteeringClient.to_numbers_dict[letter]] = value
-			response[17]=False
+			response[17] = False
 			kwargs['keys'] = response
 			return frame, kwargs
-
-
-
